638e7e9f5fdc7e7b07048b90b87025f9d5c5445e
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-11 18:32:58 +0900

    Update 대표값.py

--- section_2/대표값/대표값.py
+++ section_2/대표값/대표값.py
@@ -10,3 +10,20 @@ if min in result:
 else:
     idx = result.index(-min)
 print(mean, idx+1)
+
+######### SOLUTION ##########
+n = int(input())
+a = list(map(int, input().split()))
+ave = round(sum(a)/n)
+min=2147000000
+
+for idx, x in enumerate(a):
+    tmp = abs(x-ave)
+    if tmp < min:
+        min = tmp
+        score = x
+        res = idx + 1
+     elif tmp == min:
+        if x > score:
+            score = x
+            res = idx + 1

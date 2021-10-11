b4576f373a1c6162405205e24e71b37e784743a2
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-11 16:37:52 +0900

    Update k번째_약수.py

--- section_2/k번째_약수/k번째_약수.py
+++ section_2/k번째_약수/k번째_약수.py
@@ -11,3 +11,19 @@ def run(n, k):
 n, k = map(int, input().split())
 result = run(n,k)
 print(result)
+
+########### SOLUTION #############
+## list를 사용하지 않고 cnt 사용
+## 나도 for문을 끝까지 도는 것 보다 이 방법이 더 나은 것 같다.
+
+n, k = map(int, input().split())
+cnt = 0
+for i in range(1, n+1):
+  if n%i==0:
+    cnt += 1
+  if cnt==k:
+    print(i)
+    break
+else:       ####### for문에도 else가 있다! for문에서 break로 끝나지 않은 경우 수행
+  print(-1)
+           

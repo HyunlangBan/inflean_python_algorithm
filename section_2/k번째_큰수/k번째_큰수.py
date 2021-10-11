7a2372cd2b9795021fa516e999092ab432f8e54f
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-11 16:52:13 +0900

    Update k번째_큰수.py

--- section_2/k번째_큰수/k번째_큰수.py
+++ section_2/k번째_큰수/k번째_큰수.py
@@ -17,3 +17,18 @@ result.sort(reverse=True)
 print(result[p-1])
 
 ## 첫번째 예제만 맞춤...
+
+######## SOLUTION #########
+## 3개를 뽑을때는 중복을 제거하면 안되었었는데 제거해서 결과 값이 다르게 나와버렸다. 
+## set에서 add 쓰기
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+res = set()
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+res = list(res)
+res.sort(reverse=True)
+print(res[k-1])

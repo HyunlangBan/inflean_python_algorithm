c105b27a5d7a8a1cd289fcec6b265de62b9b27ba
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-12 23:57:53 +0900

    Create 자릿수의_합.py

---
+++ section_2/자릿수의_합/자릿수의_합.py
@@ -0,0 +1,12 @@
+def digit_sum(num):
+    result = sum(map(int, num))
+    return result
+
+n = int(input())
+array = list(input().split())
+max_sum = -1
+for i in range(n):
+    result = digit_sum(array[i])
+    if result > max_sum:
+        number = array[i]
+print(number)

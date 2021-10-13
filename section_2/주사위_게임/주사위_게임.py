547ac70fd5340cbe80ec8022a5caca1e78fea15a
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-14 00:11:02 +0900

    Create 주사위_게임.py

---
+++ section_2/주사위_게임/주사위_게임.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+def calculate(array):
+    max_point = -1
+    for i in range(n):
+        if array[i] == 3:
+            return 10000+(i*1000)
+        elif array[i] == 2:
+            return 1000+(i*100)
+        elif array[i] == 1:
+            max_point = i
+    return max_point * 100
+
+
+prize = []
+for _ in range(n):
+    array = [0]*7
+    points = list(map(int, input().split()))
+    for point in points:
+        array[point] += 1
+    prize.append(calculate(array))
+
+print(max(prize))

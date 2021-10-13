a12b2b877c38149a5615c780ce264ed761c91be8
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-14 00:47:28 +0900

    Update 뒤집은_소수.py

--- section_2/뒤집은_소수/뒤집은_소수.py
+++ section_2/뒤집은_소수/뒤집은_소수.py
@@ -5,9 +5,10 @@ def reverse(x):
 def isPrime(x):
     prime = True
     if x == 1:
-        return
+        return False ## 1 is not a prime
     x = int(x)
-    for i in range(2, x-1):
+    for i in range(2, x-1):  ## 절반까지만 돌면 된다! ex. (1*16) (2*8) (4,4) 
+                             ## for i in range(2, x//2+1)
         if not x%i:
             prime = False
 
@@ -19,3 +20,25 @@ for i in array:
     reversed = reverse(i)
     if isPrime(reversed):
         print(reversed, end = ' ')
+
+ 
+########### SOLUTION ##############
+# reverse를 수학적으로 해결
+# isPrime이 더 간결
+
+def reverse(x):
+    res=0
+    while x>0:
+        t=x%10
+        res=res*10+t
+        x=x//10
+    return res
+
+def isPrime(x):
+    if x==1:
+        return False
+    for i in range(2, x//2+1):
+        if x%i==0:
+            return False
+    else:
+        return True

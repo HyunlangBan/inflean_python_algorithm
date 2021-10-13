12e1808fcac9df2cce1167349217d941902a7169
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-13 23:53:39 +0900

    Update 뒤집은_소수.py

--- section_2/뒤집은_소수/뒤집은_소수.py
+++ section_2/뒤집은_소수/뒤집은_소수.py
@@ -1,3 +1,18 @@
+def reverse(x):
+    reversed = x[::-1]
+    return int(reversed)
+
+def isPrime(x):
+    prime = True
+    if x == 1:
+        return
+    x = int(x)
+    for i in range(2, x-1):
+        if not x%i:
+            prime = False
+
+    return prime
+
 n = input()
 array = list(input().split())
 for i in array:

a23d9b3f99555b8fc26f07f5922e341235693f86
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-15 01:30:50 +0900

    Update 카드_역배치.py

--- section_3/카드_역배치/카드_역배치.py
+++ section_3/카드_역배치/카드_역배치.py
@@ -12,3 +12,15 @@ for _ in range(10):
     
 for a in array[1:]:
     print(a, end=' '
+
+          
+########## SOLUTION ###########
+
+a = list(range(21) ## 간단하다
+for _ in range(10):
+     s, e = map(int, input().split())
+     for i in range((e-s+1)//2):
+         a[s+i], a[e-i] = a[e-i], a[s+i]  # 이렇게 swap하는걸 시도했었는데 안됐던 이유는 범위를 반으로 안나눠서이다.
+     a.pop(0)
+     for x in a:
+         print(x, end=' ')

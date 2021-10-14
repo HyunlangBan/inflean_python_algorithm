8acdc11f0290f80ae897aa6b662b2bd23325ede4
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-15 01:22:24 +0900

    Update 숫자만_추출.py

--- section_3/숫자만_추출/숫자만_추출.py
+++ section_3/숫자만_추출/숫자만_추출.py
@@ -15,3 +15,15 @@ for i in range(1, only_number+1):
 
 print(only_number)
 print(result)
+
+########### SOLUTION ############
+## .isdecimal
+## 문자->숫자 변환 계산
+
+s = input()
+res = 0
+
+for x in s:
+    if x.isdecimal():   # 0~9까지의 숫자인지 확인한다.
+        res = res*10 + int(x)
+# 이하 방법 동일

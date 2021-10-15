7006935daabc873f1902d489e711d56542d7b0e9
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-16 02:24:47 +0900

    Update 격자판_최대합.py

--- section_3/격자판_최대합/격자판_최대합.py
+++ section_3/격자판_최대합/격자판_최대합.py
@@ -23,3 +23,32 @@ if max_sum < di_sum:
     max_sum = di_sum
 
 print(max_sum)
+
+############# SOLUTION ##############
+## 훨씬 답이 간결
+## 난 대각선 반대편 계산을 빼먹었다;;
+
+a = [list(map(int, input().split())) for _ in range(n)] ## 간단하다!
+largest=-2147000000
+for i in range(n):
+    sum1=sum2=0
+    for j in range(n):
+        sum1+=a[i][j]
+        sum2+=a[j][i]
+    if sum1>largest:
+        largest=sum1
+    if sum2>largest:
+        largest=sum2
+        
+sum1=sum2=0
+for i in range(n):
+    sum1+=a[i][j]
+    sum2+=a[i][n-i-1]
+if sum1>largest:
+    largest=sum1
+if sum2>largest:
+    largest=sum2
+    
+print(largest)
+    
+       

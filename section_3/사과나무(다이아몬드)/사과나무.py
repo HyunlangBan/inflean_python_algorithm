1e0717b8cb6183793c1cc36acc08e842b347abfc
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-16 23:38:42 +0900

    Update 사과나무.py

--- section_3/사과나무(다이아몬드)/사과나무.py
+++ section_3/사과나무(다이아몬드)/사과나무.py
@@ -12,3 +12,19 @@ for row in range(mid+1):
             apples += trees[col][n-row-1]
 
 print(apples)
+
+########## SOLUTION ###########
+## 범위를 1씩 +,-함
+
+res = 0
+s=e=n//2
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i<n//2:
+        s-=1
+        e+=1
+    else:
+        s+=1
+        e-=1
+print(res)

453470f4b2e67a4bc96b489d5eaccaa47fc3cd5b
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-17 00:08:11 +0900

    Update 봉우리.py

--- section_3/봉우리/봉우리.py
+++ section_3/봉우리/봉우리.py
@@ -33,3 +33,29 @@ for i in range(1, size-1):
             visited[i][j+1] = True
 
 print(cnt)
+
+
+############ SOLUTION ################
+## 가장자리 0으로 초기화 하는 법을 알아두자
+
+n=int(input())
+a=[list(map(int, input().split())) for _ in range(n)]
+
+# 가장자리 초기화
+a.insert(0, [0]*n)
+a.append([0]*n)
+for x in a:
+    x.insert(0,0)
+    x.append(0)
+
+cnt=0
+
+# 4방향 탐색을 할 때 유용한 방법
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if all(a[i][j] > a[i+dx[k]][j+dy[k]] for k in range(4)):   ## all: 모든 조건이 참일때
+            cnt+=1
+print(cnt)

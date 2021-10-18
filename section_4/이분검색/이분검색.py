c82f9402b535ac6bc709c70ba2ca87ac682ccf2d
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-18 22:56:37 +0900

    Update 이분검색.py

--- section_4/이분검색/이분검색.py
+++ section_4/이분검색/이분검색.py
@@ -6,7 +6,6 @@ end = n
 
 while True:
     mid = (end + start) // 2
-    print(array[mid])
     if array[mid] < m:
         start = mid + 1
     elif array[mid] > m:
@@ -14,3 +13,24 @@ while True:
     else:
         print(mid + 1)
         break
+        
+# sort 안함
+# while 조건 틀림
+
+
+############ SOLUTION ##############
+
+a.sort()
+lt = 0
+rt = n-1
+
+while lt <= rt:
+    mid = (lt+rt)//2
+    if a[mid] == m:
+        print(mid+1)
+        break
+    elif a[mid] > m:
+        rt = mid-1
+    else:
+        lt = mid+1
+       

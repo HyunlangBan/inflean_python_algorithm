a3891d8764d48de8b2c2d47a7073c6386cfe3436
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-21 00:36:10 +0900

    Update 씨름선수.py

--- section_4/씨름선수/씨름선수.py
+++ section_4/씨름선수/씨름선수.py
@@ -12,3 +12,26 @@ for h, w in info:
       cnt -= 1
 
 print(cnt)
+
+
+############### SOLUTION ################
+## 키순으로 정렬
+## 맨 위를 제외하고 자신보다 키가 큰 사람들보다 몸무게가 높아야한다.
+## 이중포문을 피하기 위해서 최대값을 구하는 방식을 선택한다. (지금까지의 범위에서 최대값이 되는지)
+
+n = int(input())
+body = []
+
+for i in range(n):
+  a, b = map(int, input().split())
+  body.append((a,b))
+  
+body.sort(revers=True)
+largest = 0
+cnt = 0
+for x, y in body:
+  if y > largest:
+    largest = y
+    cnt += 1
+  
+print(cnt)

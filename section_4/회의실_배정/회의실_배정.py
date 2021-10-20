aa611963240b6092ca8b3f6bc0899e5a361a05c5
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-21 00:28:20 +0900

    Update 회의실_배정.py

--- section_4/회의실_배정/회의실_배정.py
+++ section_4/회의실_배정/회의실_배정.py
@@ -31,7 +31,7 @@ for i in range(n):
   s, e = map(int, input().split())
   meeting.append((s,e))
 
-meeting.sort(key=lambda x:(x[1], x[0])). # 끝나는 시간을 기준으로 하여 정렬
+meeting.sort(key=lambda x:(x[1], x[0]))  # 끝나는 시간을 기준으로 하여 정렬
 et = 0
 cnt = 0
 for s, e in meeting:

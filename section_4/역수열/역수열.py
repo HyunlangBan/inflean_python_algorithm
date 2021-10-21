d20cc50ebbb9e17170af302d5b59f9c7ddd8863b
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-22 01:03:58 +0900

    Update 역수열.py

--- section_4/역수열/역수열.py
+++ section_4/역수열/역수열.py
@@ -17,3 +17,15 @@ for i in range(n):
 for n in result:
   print(n, end=' ')
 print()
+
+
+############## SOLUTION ##############
+## 빈자리 찾는 다른 방법도 참고하기
+
+for i in range(n):
+  for j in range(n):
+    if a[i]==0 and seq[j]==0:
+      seq[j]=i+1
+      break
+    elif seq[j]==0:   ### 빈자리를 찾으면 값을 뺀다.
+      a[i]-=1

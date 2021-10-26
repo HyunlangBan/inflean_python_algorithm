66cc48aef38afa67f22db1f6ee983c2a2a291f15
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-27 00:48:03 +0900

    Update 후위식연산.py

--- section_5/후위식연산/후위식연산.py
+++ section_5/후위식연산/후위식연산.py
@@ -1,9 +1,9 @@
-a = list(input())
+a = list(input()). ## string이므로 list가 필요 없음
 tmp = []
 res = 0
 
 for i in a:
-  if i not in ['+', '-', '*', '/']:
+  if i not in ['+', '-', '*', '/']: ## i.isdecimal()
     tmp.append(i)
   else:
     x = tmp.pop()
@@ -12,4 +12,4 @@ for i in a:
     tmp.append(str(cal))
     res = cal
 
-print(res)
+print(res)  ## print(tmp[0])

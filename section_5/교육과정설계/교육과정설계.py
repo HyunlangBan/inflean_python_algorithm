1446d50ecd62f41c80a0306c43fd0d30fcb5cd95
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-29 00:13:33 +0900

    Update 교육과정설계.py

--- section_5/교육과정설계/교육과정설계.py
+++ section_5/교육과정설계/교육과정설계.py
@@ -20,3 +20,24 @@ for _ in range(n):
     print("NO")
     
 ## 이거보다 깔끔한 방법이 있을 것 같은 느낌....
+
+############## SOLUTION ################
+## 수업 계획에서 필수과목인지 아닌지 확인해야함
+
+need = input()
+n = int(input())
+
+for i in range(n):
+  plan = input()
+  dq = deque(need)
+  
+  for x in plan:
+    if x in dq:
+      if x != dq.popleft():
+        print("NO")
+        break
+  else:
+    if len(dq)==0:
+      print("YES")
+    else:
+      print("NO")

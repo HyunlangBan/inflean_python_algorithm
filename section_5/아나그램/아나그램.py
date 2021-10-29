ed1a5d3201819b883940feb54bcee6891d2105ae
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-29 16:45:24 +0900

    Update 아나그램.py

--- section_5/아나그램/아나그램.py
+++ section_5/아나그램/아나그램.py
@@ -18,3 +18,28 @@ if w1 == w2:
   print("YES")
 else:
   print("NO")
+
+  
+############# SOLUTION ###############
+## dict에서 get함수를 써보자
+
+a = input()
+b = input()
+str1 = dict()
+str2 = dict()
+
+for x in a:
+  str[x] = str1.get(x, 0) + 1
+for x in b:
+  str2[x] = str2.get(x, 0) + 1
+
+for i in str1.keys():
+  if i in str2.keys():
+    if str1[i] != str2[i]:
+      print("N0")
+      break
+  else:
+    print("NO")
+    break
+else:
+  print("YES")

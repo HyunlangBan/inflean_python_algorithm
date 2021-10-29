3708ed8d4e36b24ff27b2b183b2483fde88f27a2
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-29 17:15:31 +0900

    Update 최소힙.py

--- section_5/최소힙/최소힙.py
+++ section_5/최소힙/최소힙.py
@@ -7,8 +7,13 @@ while True:
   if n == -1:
     break
   elif n==0:
-    r = heapq.heappop(h)
-    res.append(r)
+    ##### 추가 필요
+    if len(a) == 0:
+      print(-1)
+    else:
+    #####
+      r = heapq.heappop(h)
+      res.append(r)
   else:
     heapq.heappush(h, n)
 

d127a18f6ea88b64e186feaf0a0e18af0d389905
Hyunlang Ban <gusfkd2827@gmail.com>
2021-10-29 16:28:27 +0900

    Create 최소힙.py

---
+++ section_5/최소힙/최소힙.py
@@ -0,0 +1,16 @@
+import heapq
+
+h = []
+res = []
+while True:
+  n = int(input())
+  if n == -1:
+    break
+  elif n==0:
+    r = heapq.heappop(h)
+    res.append(r)
+  else:
+    heapq.heappush(h, n)
+
+for i in res:
+  print(i)

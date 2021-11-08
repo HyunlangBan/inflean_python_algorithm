603a9d53641f3ccfd063a2b3cdc908648b96208d
Hyunlang Ban <gusfkd2827@gmail.com>
2021-11-09 00:24:32 +0900

    Update 동전교환.py

--- section_6/동전교환/동전교환.py
+++ section_6/동전교환/동전교환.py
@@ -27,3 +27,24 @@ DFS(0, m)
 
 ############## SOLUTION ################
 ## 중복순열과 유사한 풀이
+
+dfs DFS(L, sum):
+  global res
+  
+  if L > res:   ## Cut Edge
+    return
+  if sum > m:
+    return
+  if sum == m:
+    if L < res:
+      res = L
+      
+  else:
+    for i in range(n):
+      DFS(L+1, sum + a[i])
+      
+      
+res = 2147000000
+a.sort(reverse=True)
+DFS(0, 0)
+print(res)

e791ea31265d3deb36011bf95c1037f499d47ba6
Hyunlang Ban <gusfkd2827@gmail.com>
2021-11-09 01:05:22 +0900

    Update 순열구하기.py

--- section_6/순열구하기/순열구하기.py
+++ section_6/순열구하기/순열구하기.py
@@ -20,3 +20,25 @@ DFS(0, 0)
 
 ########### SOLUTION ############
 ## 중복을 확인하는 checklist를 만들어야함
+
+def DFS(L):
+  global cnt
+  if L==m:
+    for j in range(L):
+      print(res[j], end=' ')
+    print()
+    cnt += 1
+  
+  else:
+    for i in range(1, n+1):
+      if ch[i] == 0:
+        cn[i] = 1
+        res[L] = i
+        DFS(L+1)
+        ch[i] = 0
+
+res = [0] * n
+ch = [0] * (n+1)
+cnt = 0
+DFS(0)
+print(cnt)

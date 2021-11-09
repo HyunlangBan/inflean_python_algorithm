50d8f278dc144c1bd25e01eb0c357aa8a14583a3
Hyunlang Ban <gusfkd2827@gmail.com>
2021-11-10 01:08:18 +0900

    Update 조합구하기.py

--- section_6/조합구하기/조합구하기.py
+++ section_6/조합구하기/조합구하기.py
@@ -21,3 +21,23 @@ cnt = 0
 DFS(0)
 print(cnt)
   
+
+############### SOLUTION ################# 
+
+def DFS(L, s):
+  global cnt
+  if L==m:
+    for j in range(L):
+      print(res[j], end=' ')
+    cnt += 1
+    print()
+  else:
+    for i in range(s, n+1):
+      res[L] = i
+      DFS(L+1, i+i)
+  
+  
+res = [0] * (n+1)
+cnt = 0
+DFS(0, 1)
+print(cnt)

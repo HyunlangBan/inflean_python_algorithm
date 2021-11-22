ec39d719738a57014c4b1910ad40dba66b7600a9
Hyunlang Ban <gusfkd2827@gmail.com>
2021-11-23 01:06:48 +0900

    Update 휴가.py

--- section_7/휴가/휴가.py
+++ section_7/휴가/휴가.py
@@ -22,3 +22,30 @@ def DFS(L, idx, t, p):
 res = -2174000000
 DFS(0, 0, 0, 0)
 print(res)
+
+############# SOLUTION ##############
+## 부분집합
+## L: 인덱스 겸 누적 일수
+
+def DFS(L, sum):
+  global res
+  if L==n+1:
+    if sum > res:
+      res = sum
+    else:
+      if L+T[L] <= n+1:   # 날짜에 잡힌 상담을 진행
+        DFS(L+T[L], sum+P[L])
+      DFS(L+1, sum)
+
+n = int(input())
+T = list()
+P = list()
+for i in range(n):
+  a, b = map(int, input().split())
+  T.append(a)
+  P.append(b)
+res = -2147000000
+T.insert(0, 0)      ## idx를 날짜와 맞추기 위하여(1일부터 시작)
+P.insert(0, 0)
+DFS(1, 0)
+print(res)

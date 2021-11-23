395c43ddd8f81d1658b82dcbd82ec1949c6c866d
Hyunlang Ban <gusfkd2827@gmail.com>
2021-11-24 01:02:36 +0900

    Create 동전바꿔주기.py

---
+++ section_7/동전바꿔주기/동전바꿔주기.py
@@ -0,0 +1,22 @@
+############ SOLUTION ###############
+
+def DFS(L, sum):
+  global cnt
+  if L==k:
+    if sum==T:
+      cnt += 1
+  else:
+    for i in range(cn[L]+1):
+      DFS(L+1, sum+(cv[L]*i))
+
+T = int(input())
+k = int(input())
+cv = list()
+cn = list()
+for i in range(k):
+  a, b = map(int, input().split())
+  cv.append(a)
+  cn.append(b)
+cnt = 0
+DFS(0, 0)
+print(cnt)

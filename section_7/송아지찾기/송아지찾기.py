375cc7a84db470705e03d2351fd25865615502f8
Hyunlang Ban <gusfkd2827@gmail.com>
2021-12-01 23:57:39 +0900

    Update 송아지찾기.py

--- section_7/송아지찾기/송아지찾기.py
+++ section_7/송아지찾기/송아지찾기.py
@@ -24,3 +24,31 @@ res = 1247000000
 DFS(s, 0)
 
 print(res)
+
+
+############ SOLUTION ############
+## BFS
+
+from collections import deque
+
+MAX = 100000
+ch = [0] * (MAX+1)
+dis = [0] * (MAX+1)
+n, m = map(int, input().split())
+ch[n] = 1
+dis[n] = 0
+dQ = deque()
+dQ.append(n)
+
+while dQ:
+  now = dQ.popleft()
+  if now==m:
+    break
+  for next in (now-1, now+1, now+5):
+    if 0<next<=MAX:
+      if ch[next] == 0:
+        dQ.append(next)
+        ch[next]=1
+        dis[next]=dis[now]+1
+        
+print(dis[m])

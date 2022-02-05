0a7031fea45b8503d77f26c499178e97cc83b412
Hyunlang Ban <gusfkd2827@gmail.com>
2022-02-05 18:02:18 +0900

    Create review.py

---
+++ section_2/소수/review.py
@@ -0,0 +1,12 @@
+## Try1 => ❌❌❌ TIMEOUT ❌❌❌
+
+n = int(input())
+cnt = 0
+for i in range(2, n+1):
+  for j in range(2, i):
+    if i%j==0:
+      # print(f"{i%j}")
+      break
+  else:
+    cnt += 1
+print(cnt)

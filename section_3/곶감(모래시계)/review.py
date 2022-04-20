8817cd8f86206f12a6750314e03a5a4e193cfbe4
Hyunlang Ban <gusfkd2827@gmail.com>
2022-04-20 22:35:57 +0900

    Update review.py

--- section_3/곶감(모래시계)/review.py
+++ section_3/곶감(모래시계)/review.py
@@ -40,3 +40,34 @@ result += aa[n//2][n//2]
 print(result)
 
 ## 회전을 한바퀴 이상 하는 경우를 생각하지 못해서 오답 발생
+
+#----------------------------------------------------#
+
+# Try 2 - ✅
+# 회전쓰 ~
+# 행번호 방향(0-왼, 1-오) 격자수
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+for _ in range(m):
+  k, d, p = map(int, input().split())
+  new_k = a[k-1].copy()
+  p = p%n
+  if d == 0:
+    for i in range(n):
+      a[k-1][i-p] = new_k[i]
+  elif d == 1:
+    for i in range(n):
+      new_idx = i+p
+      if new_idx >= n:
+        new_idx -= n
+      a[k-1][new_idx] = new_k[i]
+      
+s = 0
+for i in range(n//2):
+  s += sum(a[i][i:n-i])
+  s += sum(a[n-i-1][i:n-i])
+
+s += a[n//2][n//2]
+print(s)

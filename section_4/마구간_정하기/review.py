c154ce9183653aea38c23accf0ba4d823de22866
Hyunlang Ban <gusfkd2827@gmail.com>
2022-05-06 23:32:06 +0900

    Update review.py

--- section_4/마구간_정하기/review.py
+++ section_4/마구간_정하기/review.py
@@ -33,3 +33,51 @@ while lt <= rt:
     rt = mid-1
   
 print(result)
+
+
+# Try 2 - ❌
+
+n, c = map(int, input().split())
+a = []
+for _ in range(n):
+  a.append(int(input()))
+
+a.sort()
+
+print(a)
+
+## 최대거리
+lt = 1
+rt = a[-1]
+
+def count(dist):
+  p = 0
+  cnt = 1
+  
+  # 말은 next보다 같거나 큰 마구간으로 가야한다.
+  for i in range(n):
+    next = a[p] + dist
+    print(next)
+    if a[i] == next:
+      p = i
+      cnt += 1
+    elif a[i] > next:
+      p = i+1
+      cnt += 1
+
+  return cnt
+
+result = -2147000000
+while lt<=rt:
+  mid = (lt+rt)//2
+  print(f'mid: {mid}')
+  if count(mid) <= c:
+    result = mid
+    rt = mid - 1
+  else:
+    lt = mid + 1
+
+print(result)
+
+# count 어떻게 해야하는지 모르겠음
+# while 내에서 분기하는거 자꾸 틀림 

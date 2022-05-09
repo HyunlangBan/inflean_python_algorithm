76a9c2a7e73bad811a0d437fe84aebe8a5fc7a6d
Hyunlang Ban <gusfkd2827@gmail.com>
2022-05-09 22:30:11 +0900

    Update review.py

--- section_4/씨름선수/review.py
+++ section_4/씨름선수/review.py
@@ -22,3 +22,30 @@ for i in range(n-1):
     cnt += 1
 
 print(cnt)
+
+# Try 2 - ✅
+# 무게가 가벼운 기준으로 정렬하고
+# 무게가 가벼운 선수들은 무게가 무거운 다른 선수들보다 키가 가장 커야한다.
+# 가장 무거운 선수는 무조건 선발되므로 cnt는 1부터 시작한다.
+n = int(input())
+players = []
+for _ in range(n):
+  h, w = map(int, input().split())
+  players.append((w, h))
+
+players.sort()
+# print(players)
+
+cnt = 1
+for i in range(n-1):
+  w, h = players[i][0], players[i][1]
+  max_h = h
+  for j in range(i+1, n):
+    y_w, y_h = players[j][0], players[j][1]
+    # print(h, y_h)
+    max_h = max(max_h, y_h)
+    # print(max_h)
+  if max_h ==h:
+    cnt += 1
+
+print(cnt)

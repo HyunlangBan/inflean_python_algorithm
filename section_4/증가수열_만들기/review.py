673d1d97d128886aaf51760491dc919883c05048
Hyunlang Ban <gusfkd2827@gmail.com>
2022-05-11 22:10:49 +0900

    Update review.py

--- section_4/증가수열_만들기/review.py
+++ section_4/증가수열_만들기/review.py
@@ -36,3 +36,43 @@ while True:
     break
     
 ## 적절한 해결 방법이 떠오르지 않았음 ㅜ
+
+# Try 2 - ✅
+n = int(input())
+a = list(map(int, input().split()))
+
+# 양 끝의 수들 중에 작은 값을 가져온다.
+from collections import deque
+q = deque(a)
+x = -1
+res = ''
+while q:
+  l = q[0]
+  r = q[-1]
+  
+  if x != -1 and x>l and x>r:
+    break
+  
+  if l>x and r>x:
+    if l<r:
+      res += 'L'
+      q.popleft()
+      x = l
+    else:
+      res += 'R'
+      q.pop()
+      x = r
+      
+  elif l>x and r<x:
+    res += 'L'
+    q.popleft()
+    x = l
+  elif r>x and l<x:
+    res += 'R'
+    q.pop()
+    x = r
+    
+print(len(res))
+print(res)
+
+# 어떤 경우로 나눠야할지 생각하는게 좀 어려웠다.

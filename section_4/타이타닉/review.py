731eaa91b5311b7ade364ee2808d147340a40cc3
Hyunlang Ban <gusfkd2827@gmail.com>
2022-05-11 21:50:13 +0900

    Update review.py

--- section_4/타이타닉/review.py
+++ section_4/타이타닉/review.py
@@ -17,3 +17,22 @@ while a:
   cnt += 1
 
 print(cnt)
+
+# Try 2 - ✅
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+# 한번 보낼때 최대한 두명으로 맞춰서 보내기
+a.sort(reverse=True)
+
+cnt = 0
+while a:
+  print(a)
+  p = a.pop(0)
+  cnt += 1
+  for i in range(len(a)):
+    if p + a[i] <= m:
+      a.remove(a[i])
+      break
+
+print(cnt)

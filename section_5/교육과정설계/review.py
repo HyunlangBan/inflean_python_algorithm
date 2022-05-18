0cbb976ffe77faf49c96a0f9064313009bdf47a0
Hyunlang Ban <gusfkd2827@gmail.com>
2022-05-19 07:38:14 +0900

    Update review.py

--- section_5/교육과정설계/review.py
+++ section_5/교육과정설계/review.py
@@ -12,3 +12,26 @@ for _ in range(n):
         print("YES")
     else:
         print("NO")
+
+
+# Try 2 - ✅
+req = input()
+n = int(input())
+
+res = []
+
+for _ in range(n):
+  c = input()
+  y = list(req)
+  for x in c:
+    if y and x == y[0]:
+      y.pop(0)
+
+    if not y:
+      res.append("YES")
+      break
+  else:
+    res.append("NO")
+
+for i, s in enumerate(res, 1):
+  print(f'#{i} {s}')

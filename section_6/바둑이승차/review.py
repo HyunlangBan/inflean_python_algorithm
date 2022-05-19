c7587f78fcf93e2de0d649cdfeacf64675d3b9e8
Hyunlang Ban <gusfkd2827@gmail.com>
2022-05-20 08:03:41 +0900

    Update review.py

--- section_6/바둑이승차/review.py
+++ section_6/바둑이승차/review.py
@@ -20,3 +20,30 @@ def DFS(idx, sum):
   DFS(idx+1, sum)
 
 DFS(0, 0)
+
+# Try 2 - ✅
+c, n = map(int, input().split())
+a = [int(input()) for _ in range(n)]
+
+### 가장 무거운 부분 집합을 구
+res = 0
+total = sum(a)
+
+def DFS(L, s):
+  global res
+  
+  # cut-edge
+  if s + (total-s) <= res:
+    return
+  if s > c:
+    return
+  if L==n:
+    if s > res:
+      res = s
+    return
+
+  DFS(L+1, s+a[L])
+  DFS(L+1, s)
+
+DFS(0, 0)
+print(res)

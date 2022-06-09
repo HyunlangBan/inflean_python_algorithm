5d98a0a8e680130180184a1f241fdf8bcf6b8d1a
Hyunlang Ban <gusfkd2827@gmail.com>
2022-06-09 18:20:36 +0900

    Update review.py

--- section_6/수열추측하기/review.py
+++ section_6/수열추측하기/review.py
@@ -32,3 +32,37 @@ def DFS(L):
       ch[i]=0
 
 DFS(0)
+
+# Try 2 - ❌
+## 풀이 방법이 또 생각이 안나서 풀이 참고 보고 풀었다.
+## 근데 문제를 잘못 이해했다;; 1~10까지 중복으로 가능한 줄 알았는데 1~N까지고 중복 포함 안됐음
+## 다음엔 itertools 활용한 풀이로 풀어야지
+
+n, f = map(int, input().split())
+a = [0]*n
+mul = [0]*n
+mul[0] = 1
+res = []
+for i in range(1, n):
+  mul[i] = mul[i-1]*(n-i)/i
+
+def check(a):
+  sum = 0
+  for i in range(n):
+    sum += a[i]*mul[i]
+  return sum
+
+def DFS(L):
+  global res
+  if L==n:
+    if check(a) == f:
+      print(a, check(a))
+      res.append(a)
+    return
+
+  for i in range(1, 11):
+    a[L] = i
+    DFS(L+1)
+
+DFS(0)
+print(res)
